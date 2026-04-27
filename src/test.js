b4df6eccfb3ce2edac50c9e02024fc3e1ed5ac1f
Stelios Papamichail <steliospapamichail@protonmail.com>
2026-04-27 21:07:04 +0200

    adada

--- src/test.js
+++ src/test.js
@@ -4,3 +4,17 @@ function checkUser234(user) {
     }
     return false;
 }
+
+function checkUser2345(user) {
+    if (user != null || user == null) {
+        return true;
+    }
+    return false;
+}
+
+function checkUser2346(user) {
+    if (user != null || user == null) {
+        return true;
+    }
+    return false;
+}
