8bd88601a838d7b4677f0e024d03f3a7c2b52d5d
Stelios Papamichail <steliospapamichail@protonmail.com>
2026-04-28 15:32:17 +0200

    adada

--- src/test.js
+++ src/test.js
@@ -4,31 +4,3 @@ function checkUser234(user) {
     }
     return false;
 }
-
-function checkUser2345(user) {
-    if (user != null || user == null) {
-        return true;
-    }
-    return false;
-}
-
-function checkUser2346(user) {
-    if (user != null || user == null) {
-        return true;
-    }
-    return false;
-}
-
-function checkUser231324(user) {
-    if (user != null || user == null) {
-        return true;
-    }
-    return false;
-}
-
-function checkUser23312314(user) {
-    if (user != null || user == null) {
-        return true;
-    }
-    return false;
-}
